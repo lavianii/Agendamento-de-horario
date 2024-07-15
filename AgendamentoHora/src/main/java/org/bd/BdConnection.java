e698e039a5dab7d1f5aeb1540460a432d276f533
lavianii <jonaslavi02@gmail.com>
2024-07-15 12:25:14 -0300

    refact: removendo credenciais

--- AgendamentoHora/src/main/java/org/bd/BdConnection.java
+++ AgendamentoHora/src/main/java/org/bd/BdConnection.java
@@ -6,8 +6,8 @@ public class BdConnection {
     public Connection conectBd() {
         Connection connection = null;
         try {
-            String url = "jdbc:mysql://127.0.0.2:3306/agendamentohora";
-            connection = DriverManager.getConnection(url, "root", "1234");
+            String url = "jdbc:mysql://127.0.0.2:3306/seu-bd";
+            connection = DriverManager.getConnection(url, "seu-usuario", "sua-senha");
 
         } catch (SQLException sqlException) {
             System.out.println("Houve um problema ao se conectar com o BANCO DE DADOS. " + sqlException.getMessage());
